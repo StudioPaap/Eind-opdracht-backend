663c78095c619afc96a16c926a535ea370a790d1
StudioPaap <anoukpaap@gmail.com>
2023-04-02 15:32:17 +0200

    werken aan opdracht deel 1

---
+++ src/main/java/com/novi/eindopdracht_novi_anoukpaap/EindOpdrachtNoviAnoukPaapApplication.java
@@ -0,0 +1,13 @@
+package com.novi.eindopdracht_novi_anoukpaap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindOpdrachtNoviAnoukPaapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindOpdrachtNoviAnoukPaapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/novi/eindopdracht_novi_anoukpaap/EindOpdrachtNoviAnoukPaapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novi.eindopdracht_novi_anoukpaap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindOpdrachtNoviAnoukPaapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
